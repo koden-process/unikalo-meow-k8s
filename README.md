b0730d3d9f6747dbcf4e01fda3f4570c57799b40
Killian KOPP <killiankopp@gmail.com>
2025-03-23 17:10:28 +0100

    déploiement

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# sample-meow-deploy
+# meow-k8s
+
+Pour déployer ces charts en utilisant ArgoCD
+
+
+```sh
+kubectl apply -f argocd-app.yaml -n argocd
+```
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+read -p "Entrez le nom du repo (REPO) ex : user/repo : " REPO
+read -p "Entrez le nom de l'application (APP_NAME) : " APP_NAME
+read -p "Entrez le nom du projet ArgoCD (PROJECT_NAME) : " PROJECT_NAME
+read -p "Entrez le nom du secret Kubernetes (SECRET_NAME) : " SECRET_NAME
+read -p "Entrez le namespace Kubernetes (NAMESPACE) : " NAMESPACE
+read -p "Entrez l'URL' (URL) : " URL
+
+sed -i '' "s/APP_NAME/$APP_NAME/g" app.yaml
+sed -i '' "s/PROJECT_NAME/$PROJECT_NAME/g" app.yaml
+sed -i '' "s/NAMESPACE/$NAMESPACE/g" app.yaml
+
+
+sed -i '' "s/APP_NAME/$APP_NAME/g" charts/values.yaml
+sed -i '' "s/PROJECT_NAME/$PROJECT_NAME/g" charts/values.yaml
+sed -i '' "s/NAMESPACE/$NAMESPACE/g" charts/values.yaml
+sed -i '' "s/SECRET_NAME/$SECRET_NAME/g" charts/values.yaml
+
+kubectl apply -f app.yaml -n argocd
+
+echo "Déploiement terminé avec succès."
+
+git add .
+git commit -m "Update app.yaml"
+git push
+
+echo "Les fichiers ont été mis à jour sur Github."
